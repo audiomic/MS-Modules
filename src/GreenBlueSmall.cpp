9ad62b4ce820c143b1e32272f83b0f978bb74888
Phal-anx <31989694+Phal-anx@users.noreply.github.com>
2017-10-08 01:39:29 +0200

    Add files via upload

---
+++ src/GreenBlueSmall.cpp
@@ -0,0 +1,34 @@
+#include "app.hpp"
+
+namespace rack {
+
+////////////////////
+// Colors
+////////////////////
+
+#define COLOR_BLACK_TRANSPARENT nvgRGBA(0x00, 0x00, 0x00, 0x00)
+#define COLOR_BLACK nvgRGB(0x00, 0x00, 0x00)
+#define COLOR_WHITE nvgRGB(0xff, 0xff, 0xff)
+#define COLOR_RED nvgRGB(0xed, 0x2c, 0x24)
+#define COLOR_ORANGE nvgRGB(0xf2, 0xb1, 0x20)
+#define COLOR_YELLOW nvgRGB(0xf9, 0xdf, 0x1c)
+#define COLOR_GREEN nvgRGB(0x90, 0xc7, 0x3e)
+#define COLOR_CYAN nvgRGB(0x22, 0xe6, 0xef)
+#define COLOR_BLUE nvgRGB(0x29, 0xb2, 0xef)
+#define COLOR_PURPLE nvgRGB(0xd5, 0x2b, 0xed)
+
+////////////////////
+// Knob
+////////////////////
+
+struct GreenBlueSmallKnob : SVGKnob {
+	GreenBlueSmallKnob() {
+		SVGKnob(Knob);
+		box.size = Vec(36, 36);
+		minAngle = -0.83*M_PI;
+		maxAngle = 0.83*M_PI;
+		setSVG(SVG::load("plugins/MS_modules/res/GreenBlueSmall.svg"));
+	}
+}; 
+
+} // namespace rack
