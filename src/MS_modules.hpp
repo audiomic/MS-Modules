26d7dd8697fbd3cdc953335aac249f2fc751359e
Phal-anx <31989694+Phal-anx@users.noreply.github.com>
2017-10-12 16:24:56 +0200

    Update MS_modules.hpp

--- src/MS_modules.hpp
+++ src/MS_modules.hpp
@@ -1,9 +1,48 @@
 #include "rack.hpp"
+#include "app.hpp"
 
 using namespace rack;
 
 extern Plugin *plugin;
 
+////////////////////
+// Knob
+////////////////////
+
+struct GreyKnob : SVGKnob {
+	GreyKnob() {
+		minAngle = -0.83*M_PI;
+		maxAngle = 0.83*M_PI;
+	}
+};
+
+
+struct GreyLargeKnob : GreyKnob {
+	GreyLargeKnob() {
+		box.size = Vec(46, 46);
+		setSVG(SVG::load(assetPlugin(plugin, "res/GreyLargeKnob.svg")));
+	}
+}; 
+
+struct GreySmallKnob : GreyKnob {
+	GreySmallKnob() {
+		box.size = Vec(30, 30);
+		setSVG(SVG::load(assetPlugin(plugin, "res/GreySmallKnob.svg")));
+	}
+}; 
+
+/////////////////////////
+// Jacks
+/////////////////////////
+
+struct SilverSixPort : SVGPort {
+	SilverSixPort() {
+		background->svg = SVG::load(assetPlugin(plugin, "res/SilverSixPort.svg"));
+		background->wrap();
+		box.size = background->box.size;
+	}
+	
+};
 
 ////////////////////
 // module widgets
