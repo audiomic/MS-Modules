7aefe1a9bdb0928daa7ae528322c48497b062ebb
Phal-anx <31989694+Phal-anx@users.noreply.github.com>
2017-10-12 16:25:46 +0200

    Update RandomSource.cpp

--- src/RandomSource.cpp
+++ src/RandomSource.cpp
@@ -1,6 +1,4 @@
 #include "MS_modules.hpp"
-#include "GreenBlueKnob.cpp"
-
 
 #define DTRIG 7000.0
 
@@ -28,39 +26,31 @@ struct RandomSource : Module {
 		float sample = 0.0;
 		float lights[1] = {};
 				
-		RandomSource();
+		RandomSource() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS) {}
 		void step();
 };
 
-
-RandomSource::RandomSource() {
-	params.resize(NUM_PARAMS);
-	inputs.resize(NUM_INPUTS);
-	outputs.resize(NUM_OUTPUTS);
-}
-
-	
 void RandomSource::step() {
 	
 	//sample and hold
-	float range_knob = params[RANGE_PARAM] / 15.0;
-   	float range_cv = getf(inputs[CV_INPUT]) * 12.0;
-	float range_cv_amt = params[CV_PARAM];
-	float range = range_knob + range_cv_amt * range_cv;
-	float clock = getf(inputs[TRIG_INPUT]) ;
+	float range_knob = params[RANGE_PARAM].value * 6.0;
+    float range_cv = inputs[CV_INPUT].value * 24.0 / 12.0; //before it was *12
+    float range_cv_amt = params[CV_PARAM].value;
+    float range = range_knob + range_cv_amt * range_cv;
+	float clock = inputs[TRIG_INPUT].value;
 	float dtrig = (clock - lastTrig) * gSampleRate;
 	if (dtrig > DTRIG) {
-		sample = getf(inputs[SH_INPUT]) + range;
+		sample = inputs[SH_INPUT].normalize(range);
 	}
 	lastTrig = clock;
 	
-	float SHOut = sample * params[RANGE_PARAM] / 10.0;
+	float SHOut = sample * params[RANGE_PARAM].value / 1.5;
 	
 	//light
 	lights[1] = sample;
 		
 	//Output
-	setf(outputs[SH_OUTPUT], SHOut);
+	outputs[SH_OUTPUT].value = SHOut;
 		
 
 }
@@ -73,7 +63,7 @@ RandomSourceWidget::RandomSourceWidget() {
 		{
 			SVGPanel *panel = new SVGPanel();
 			panel->box.size = box.size;
-			panel->setBackground(SVG::load("plugins/MS_modules/res/Random Source.svg"));
+			panel->setBackground(SVG::load(assetPlugin(plugin, "/res/Random Source.svg")));
 			addChild(panel);
 		}
 		
@@ -82,18 +72,17 @@ RandomSourceWidget::RandomSourceWidget() {
 		addChild(createScrew<ScrewSilver>(Vec(15, 365)));
 		
 		//Param
-		addParam(createParam<GreenBlueLargeKnob>(Vec(7, 90), module, RandomSource::RANGE_PARAM, 0.0, 1.0, 0.0));
-		addParam(createParam<GreenBlueSmallKnob>(Vec(5, 146), module, RandomSource::CV_PARAM, 0.0, 1.0, 0.0));
+		addParam(createParam<GreyLargeKnob>(Vec(7, 90), module, RandomSource::RANGE_PARAM, 0.0, 1.0, 0.0));
+		addParam(createParam<GreySmallKnob>(Vec(5, 146), module, RandomSource::CV_PARAM, 0.0, 1.0, 0.0));
 		//Inputs
-		addInput(createInput<PJ3410Port>(Vec(25, 189), module, RandomSource::CV_INPUT));
-		addInput(createInput<PJ3410Port>(Vec(25, 233), module, RandomSource::SH_INPUT));
-		addInput(createInput<PJ3410Port>(Vec(25, 279), module, RandomSource::TRIG_INPUT));
+		addInput(createInput<SilverSixPort>(Vec(29, 192), module, RandomSource::CV_INPUT));
+		addInput(createInput<SilverSixPort>(Vec(29, 237), module, RandomSource::SH_INPUT));
+		addInput(createInput<SilverSixPort>(Vec(29, 283), module, RandomSource::TRIG_INPUT));
 		
 		//Outputs
-		addOutput(createOutput<PJ3410Port>(Vec(25, 324), module, RandomSource::SH_OUTPUT));
+		addOutput(createOutput<SilverSixPort>(Vec(29, 328), module, RandomSource::SH_OUTPUT));
 		
 		//Light
-		addChild(createValueLight<SmallLight<GreenRedPolarityLight>>(Vec(48, 156), &module->lights[1]));
+		addChild(createValueLight<SmallLight<GreenRedPolarityLight>>(Vec(44, 156), &module->lights[1]));
 		
-}
-
+};
