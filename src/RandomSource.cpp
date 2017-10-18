5be070bb9211f7ad7f2c8c38a697db2a2753706b
Phal-anx <31989694+Phal-anx@users.noreply.github.com>
2017-10-18 12:05:28 +0200

    Update RandomSource.cpp

--- src/RandomSource.cpp
+++ src/RandomSource.cpp
@@ -1,26 +1,26 @@
 #include "MS_modules.hpp"
-
+#include "dsp/digital.hpp"
 
 struct RandomSource : Module {
 		enum ParamIds {
-			RANGE_PARAM,
-			NUM_PARAMS
-		};
+		RANGE_PARAM,
+		NUM_PARAMS
+	};
 		
 		enum InputIds {
-			TRIG_INPUT,
-			SH_INPUT,
-			NUM_INPUTS
-		};
+		TRIG_INPUT,
+		SH_INPUT,
+		NUM_INPUTS
+	};
 		
 		enum OutputIds {
-			SH_OUTPUT,
-			NUM_OUTPUTS
-		};
+		SH_OUTPUT,
+		NUM_OUTPUTS
+	};
 		
 		SchmittTrigger trigger;
 		float sample = 0.0;
-		float lights[1] = {};
+		float SH_Light = {};
 				
 		RandomSource() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS) {
 			trigger.setThresholds(0.0,0.7);
@@ -32,25 +32,25 @@ void RandomSource::step() {
 	
 	//sample and hold
 	float range = params[RANGE_PARAM].value;
-    		
+ 		
 	if (trigger.process(inputs[TRIG_INPUT].value)) {
 		sample = inputs[SH_INPUT].normalize(range);
-			
-	float SHOut = sample * params[RANGE_PARAM].value / 1.5;
+	}
+	
+	float SHOut = sample * params[RANGE_PARAM].value;
 	
 	//light
-	lights[1] = sample;
+	SH_Light = sample;
 		
 	//Output
 	outputs[SH_OUTPUT].value = SHOut;
-		
-	}
+
 };
 
 RandomSourceWidget::RandomSourceWidget() {
 		RandomSource *module = new RandomSource();
 		setModule(module);
-		box.size = Vec(15*4, 380);
+		box.size = Vec(60, 380);
 				
 		{
 			SVGPanel *panel = new SVGPanel();
@@ -65,7 +65,7 @@ RandomSourceWidget::RandomSourceWidget() {
 		
 		//Param
 		addParam(createParam<GreyLargeKnob>(Vec(7, 90), module, RandomSource::RANGE_PARAM, 0.0, 1.0, 0.0));
-		
+				
 		//Inputs
 		addInput(createInput<SilverSixPort>(Vec(29, 237), module, RandomSource::SH_INPUT));
 		addInput(createInput<SilverSixPort>(Vec(29, 283), module, RandomSource::TRIG_INPUT));
@@ -74,6 +74,6 @@ RandomSourceWidget::RandomSourceWidget() {
 		addOutput(createOutput<SilverSixPort>(Vec(29, 328), module, RandomSource::SH_OUTPUT));
 		
 		//Light
-		addChild(createValueLight<SmallLight<GreenRedPolarityLight>>(Vec(44, 156), &module->lights[1]));
+		addChild(createValueLight<SmallLight<GreenRedPolarityLight>>(Vec(26, 156), &module->SH_Light));
 		
 };
